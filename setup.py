a8957a8c9840e3cb197cdf6f03c419da7c0254e2
Eren Aydemir <eren.aydemir@gmail.com>
2024-06-22 00:58:35 +0300

    Update for compute capabilities of a100 and v100

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+cxx_args = ['-std=c++14']
+
+nvcc_args = [
+    '-gencode', 'arch=compute_70,code=compute_70',
+    '-gencode', 'arch=compute_80,code=compute_80'
+]
+
 setup(
     name="fused_conv_random_k",
     ext_modules=[
         CUDAExtension(
             "fused_conv_random_k_cuda",
-            ["fused_conv_g.cpp", "fused_conv_go.cu"],
-            extra_compile_args={'cxx': ['-g'],
-                                'nvcc': ['-O2']})
+            ["fused_conv_g.cpp", "fused_conv_go.cu"], 
+            extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args})
     ],
     cmdclass={
         "build_ext": BuildExtension
